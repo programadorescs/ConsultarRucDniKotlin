15befe0bb59ed4f7c57b4c442740e569402d63b2
Jack Chávez Saravia <jacksoft@live.com>
2020-11-13 11:21:29 -0500

    Se reubico las clases de en las entidades

--- app/src/main/java/com/pcs/consultarrucdnikotlin/Entidades/ApiPeruDev/ApiPeruDevData_Dni.kt
+++ app/src/main/java/com/pcs/consultarrucdnikotlin/Entidades/ApiPeruDev/ApiPeruDevData_Dni.kt
@@ -7,4 +7,23 @@ class ApiPeruDevData_Dni {
     var success: String = ""
     @SerializedName("data")
     var data: ApiPeruDev_Dni? = null
+
+    inner class ApiPeruDev_Dni {
+        @SerializedName("numero")
+        var numero: String = ""
+        @SerializedName("nombre_completo")
+        var nombre_completo: String = ""
+        @SerializedName("nombres")
+        var nombres: String = ""
+        @SerializedName("apellido_paterno")
+        var apellido_paterno: String = ""
+        @SerializedName("apellido_materno")
+        var apellido_materno: String = ""
+        @SerializedName("codigo_verificacion")
+        var codigo_verificacion: String = ""
+        @SerializedName("fecha_nacimiento")
+        var fecha_nacimiento: String = ""
+        @SerializedName("sexo")
+        var sexo: String = ""
+    }
 }
--- app/src/main/java/com/pcs/consultarrucdnikotlin/Entidades/ApiPeruDev/ApiPeruDevData_Ruc.kt
+++ app/src/main/java/com/pcs/consultarrucdnikotlin/Entidades/ApiPeruDev/ApiPeruDevData_Ruc.kt
@@ -7,4 +7,21 @@ class ApiPeruDevData_Ruc {
     var success: String = ""
     @SerializedName("data")
     var data: ApiPeruDev_Ruc? = null
+
+    inner class ApiPeruDev_Ruc {
+        @SerializedName("ruc")
+        var ruc: String = ""
+        @SerializedName("nombre_o_razon_social")
+        var nombre_o_razon_social: String = ""
+        @SerializedName("nombre_comercial")
+        var nombre_comercial: String = ""
+        @SerializedName("estado")
+        var estado: String = ""
+        @SerializedName("condicion")
+        var condicion: String = ""
+        @SerializedName("direccion")
+        var direccion: String = ""
+        @SerializedName("direccion_completa")
+        var direccion_completa: String = ""
+    }
 }
